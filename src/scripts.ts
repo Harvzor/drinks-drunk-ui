d8b4bcaba6a77a3e716056fdbc653dcf64649515
Harvey <harveyjwilliams@aol.co.uk>
2020-10-15 00:49:26 +0200

    Second chart, not based off of API data yet.

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,7 +1,10 @@
 // import Chart from 'chart.js'
+// import _ from 'lodash'
 
-const ctx: any = document.getElementById('chart')
-
+interface DrinkDrank {
+    drinkId: number
+    timestamp: Date
+}
 
 interface Drink {
     id: number
@@ -12,9 +15,6 @@ interface Drink {
 
 class DrinksDrunkApi {
     private _domain = 'http://localhost:8000'
-    constructor() {
-        
-    }
     async list(): Promise<Drink[]> {
         return await fetch(this._domain + "/drinks", {
             mode: 'cors'
@@ -25,14 +25,40 @@ class DrinksDrunkApi {
             console.error(reason)
         })
     }
+    async drinkDranks(drinkId?: number): Promise<DrinkDrank[]> {
+        function getRandomInt(max: number): number {
+            return Math.floor(Math.random() * Math.floor(max));
+        }
+
+        const date = new Date()
+
+        let data: DrinkDrank[] = []
+        
+
+        for (let i = 0; i < 20; i++) {
+            data.push({
+                drinkId: getRandomInt(5),
+                timestamp: new Date(date.setHours(date.getHours() - getRandomInt(3)))
+            })
+        }
+
+        if (drinkId != null) {
+            data = data
+                .filter(x => x.drinkId == drinkId)
+        }
+
+        return data;
+    }
 }
 
 class Page {
     private _drinkDrunkApi = new DrinksDrunkApi()
-    constructor() {
-
-    }
     async build() {
+        this.allDrinks()
+        this.drinkDranks()
+    }
+    async allDrinks() {
+        const ctx: any = document.getElementById('all-drinks')
         const drinks = await this._drinkDrunkApi.list()
 
         new Chart(ctx, {
@@ -47,6 +73,68 @@ class Page {
             },
             options: {
                 scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1,
+                        }
+                    }]
+                }
+            }
+        })
+    }
+    async drinkDranks() {
+        const ctx: any = document.getElementById('drink-dranks')
+        const data = await this._drinkDrunkApi.drinkDranks()
+
+        interface TimestampCount {
+            timestamp: Date,
+            count: number
+        }
+
+        const timestampCounts: TimestampCount[] = []
+
+        data.forEach(x => {
+            let item = timestampCounts.find(timestampCount => timestampCount.timestamp.getTime() == x.timestamp.getTime())
+
+            if (item != null) {
+                item.count++;
+            } else {
+                timestampCounts.push({
+                    timestamp: x.timestamp,
+                    count: 1,
+                })
+            }
+        })
+
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: timestampCounts.map(x => x.timestamp),
+                datasets: [{
+                    label: 'Drinks times',
+                    data: timestampCounts.map(x => {
+                        return {
+                            x: x.timestamp,
+                            y: x.count,
+                        }
+                    }),
+                }]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        distribution: 'series',
+                        // offset: true,
+                        time: {
+                            unit: 'hour'
+                        },
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1,
+                        }
+                    }],
                     yAxes: [{
                         ticks: {
                             beginAtZero: true,
