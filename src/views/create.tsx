0ddc8a797791fb3c89d6dc3a5383cbe2c7ffa504
Harvey <harveyjwilliams@aol.co.uk>
2021-01-25 02:37:42 +0100

    Made the add item menu a bit nicer.

--- src/views/create.tsx
+++ src/views/create.tsx
@@ -5,6 +5,14 @@ import { Api, Item } from "../api"
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+import Skeleton from '@material-ui/lab/Skeleton';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -17,7 +25,11 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 )
 
+
+
 export function Create() {
+    const [loading, setLoading] = React.useState(true)
+    const [items, setItems] = React.useState([])
     const [itemName, setItemName] = React.useState('')
     const [itemColour, setItemColour] = React.useState('')
 
@@ -25,27 +37,78 @@ export function Create() {
 
     const api = new Api()
 
-    const handleSubmit= (e: React.FormEvent) => {
+    console.log('load')
+
+    const loadAndRenderItems = () => {
+        api.listItems()
+            .then((returnedItems) => {
+                setItems(returnedItems)
+
+                setLoading(false)
+            })
+    }
+
+    React.useEffect(() => {
+        loadAndRenderItems()
+    }, [])
+
+    const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
 
         api.createItem(itemName, itemColour)
+            .then(() => {
+                loadAndRenderItems()
+            })
     }
 
     return (
         <>
             <h1>Create</h1>
             <form className={classes.root} onSubmit={handleSubmit}>
-                <div>
-                    <TextField name="itemName" value={itemName} onChange={e => setItemName(e.target.value)} label="Name" variant="outlined" />
-                </div>
-                <div>
-                    <TextField name="itemColour" value={itemColour} onChange={e => setItemColour(e.target.value)} label="Colour" variant="outlined" />
-                </div>
-                <div>
-                    <Button variant="contained" color="primary" type="submit">
-                        Submit
-                    </Button>
-                </div>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableBody>
+                        {loading
+                            ? <>
+                                { [...Array(10)].map((_, index) =>
+                                    <TableRow key={index}>
+                                        <TableCell component="th" scope="row">
+                                            <Skeleton key={index} />
+                                        </TableCell>
+                                    </TableRow>
+                                )}
+                            </>
+                            : <>
+                                {items.map((item: Item) => (
+                                    <TableRow key={item.id}>
+                                        <TableCell component="th" scope="row">
+                                            <span style={{ color: item.colour }}>
+                                                { item.name }
+                                            </span>
+                                        </TableCell>
+                                        <TableCell>
+                                            <span>
+                                                { item.colour }
+                                            </span>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        <TextField name="itemName" value={itemName} onChange={e => setItemName(e.target.value)} label="Name" variant="outlined" />
+                                    </TableCell>
+                                    <TableCell>
+                                        <TextField name="itemColour" value={itemColour} onChange={e => setItemColour(e.target.value)} label="Colour" variant="outlined" />
+                                        <Button variant="contained" color="primary" type="submit">
+                                            Submit
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            </>
+                        }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </form>
         </>
     )
